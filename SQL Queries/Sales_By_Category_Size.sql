ac6d41689363c158223d512fdfcceebfb566c94a
Nasrin Khatoon <nasrinkhatoon42092@gmail.com>
2026-05-21 19:54:51 +0530

    Add files via upload

---
+++ SQL Queries/Sales_By_Category_Size.sql
@@ -0,0 +1,23 @@
+SELECT * FROM pizza_sales
+
+--D. % of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+--E. % of Sales by Pizza Size
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+--F. Total Pizzas Sold by Pizza Category
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales
+WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC
+
+
